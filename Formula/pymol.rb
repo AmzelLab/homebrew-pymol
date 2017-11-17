75a083785360d43a0c73e81e99fde0cdec86fe80
telamonian <mklein@jhu.edu>
2017-11-17 17:14:54 -0500

    Added sha for patch

--- Formula/pymol.rb
+++ Formula/pymol.rb
@@ -33,9 +33,7 @@ class Pymol < Formula
   # https://github.com/Homebrew/homebrew-science/issues/5505 (04.27.17), in which bad GUI calls were causing segfaults.
   patch do
     url "https://raw.githubusercontent.com/telamonian/homebrew-pymol/master/Patch/pymol-v3.patch"
-    #url "https://sourceforge.net/p/pymol/patches/_discuss/thread/b4038fa6/f094/attachment/tk-aqua-v2.patch"
-    #url "https://gist.githubusercontent.com/telamonian/494150cf2992d324272de9a0ded78e37/raw/9abce6d6f97b3943856b63f83ad5b7352c339926/osx_aqua_ext_gui_fix.diff"
-    #sha256 "14cef03169290500a8232c9ab78964fa169bcd0854a01ced95ccbe55bd70e2c6"
+    sha256 "8bc0babbb72e8a6b112f7d34faa6e5e4eb5b1389cee754f2e530e7c81fde2d5e"
   end
 
   def install
